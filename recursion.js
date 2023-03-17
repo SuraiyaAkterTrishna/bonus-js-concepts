850d27c6e1f808f3da392c828fbd023e3070ba4f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-17 17:49:24 +0600

    factorial using recursive function

--- recursion.js
+++ recursion.js
@@ -3,7 +3,7 @@
     Recursion makes the code clean, simpler and shorter. */
 
 /* 
-// Calculate sum using for loop
+// Calculate sum of numbers using for loop
 let sum = 0; 
 for(let i = 5; i >= 1; i--){
     sum = sum + i;
