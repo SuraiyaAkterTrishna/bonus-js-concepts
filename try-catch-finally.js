89a5c7f2dfac94997987d654f1d0837afdf39216
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-11 13:08:43 +0600

    try catch finally

---
+++ try-catch-finally.js
@@ -0,0 +1,22 @@
+console.log(23);
+console.log('Hi');
+
+/* console.log(district);
+let district = 'bBaria'; */
+
+console.log(100);
+console.log(300);
+
+try{
+    console.log(district);
+    console.log('inside try');
+}
+catch(error){
+    console.log('inside catch');
+}
+finally{
+    console.log('finally done');
+}
+console.log(100);
+console.log(100);
+console.log(100);
